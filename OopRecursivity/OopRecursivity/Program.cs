c2e0fdd5c68b492df551fca78c8f59b1cf685a3f
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-02-09 19:18:09 +0200

    Implement factorial and array iteration with recursivity

---
+++ OopRecursivity/OopRecursivity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OopRecursivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursivityHelper.PrintArray(new[] { 1, 2, 3, 4, 5 });
+
+            /*
+            int n = 5;
+            int result = RecursivityHelper.Factorial(n);
+            Console.WriteLine($"Fact({n})={result}");
+            */
+        }
+    }
+}
---
+++ OopRecursivity/OopRecursivity/RecursivityHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopRecursivity
+{
+    public static class RecursivityHelper
+    {
+        public static int Factorial(int n)
+        {
+            if (n ==1 || n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return;
+            }
+
+            PrintArray(array, 0);
+        }
+
+        private static void PrintArray(int[] array, int index)
+        {
+            Console.Write(array[index]);
+
+            if (index < array.Length - 1)
+            {
+                Console.Write(", ");
+                PrintArray(array, index + 1);
+            }
+        }
+    }
+}
